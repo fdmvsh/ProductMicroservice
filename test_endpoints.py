2ecf7013b3649b4e4dfb83cf02c7c7598ef59eb6
fdmvsh <miroslav@mestan.org>
2020-01-28 02:28:10 +0100

    Update test_endpoints.py

--- test_endpoints.py
+++ test_endpoints.py
@@ -1,46 +1,43 @@
 import pytest
 import requests
 
-url = 'http://127.0.0.1:5000' # The root url of the flask app
+url = 'http://127.0.0.1:5000'
 
-def test_index_page():
-	r = requests.get(url+'/api/v1/products') # Assumses that it has a path of "/"
-	assert r.status_code == 200 # Assumes that it will return a 200 response
 
 def test_product_create():
-	global namee, descriptione, idd
-	namee = "dsfdsf"
-	descriptione = "Need to find a good Python tutorial on the web"
-	r = requests.post(url+'/api/v1/products',
-			json = {'name': namee,
-				'description': descriptione}) # Assumses that it has a path of "/"
-	idd = r.json()['id']
-	assert r.status_code == 201 # Assumes that it will return a 200 response
+	global pname, pdescription, pid
+	pname = "Example product"
+	pdescription = "Example description"
+	r = requests.post(url + '/api/v1/products',
+			json = {'name': pname,
+				'description': pdescription})
+	pid = r.json()['id']
+	assert r.status_code == 201
 
 def test_product_read():
-	r = requests.get(url+'/api/v1/products/'+str(idd))
+	r = requests.get(url + '/api/v1/products/' + str(pid))
 	assert r.status_code == 200
-	assert r.json()['name'] == namee
-	assert r.json()['description'] == descriptione
+	assert r.json()['name'] == pname
+	assert r.json()['description'] == pdescription
 
 def test_product_all_read():
-	r = requests.get(url+'/api/v1/products')
+	r = requests.get(url + '/api/v1/products')
 	assert r.status_code == 200
 	assert len(r.json()) > 0
 
 def test_product_update():
-	namee = "dsf"
-	descriptione = "ddsf"
-	r = requests.put(url+'/api/v1/products/'+str(idd),
-			json = {'name': namee,
-				'description': descriptione}) # Assumses that it has a path of "/"
-	r = requests.get(url+'/api/v1/products/'+str(idd))
-	assert r.status_code == 200 # Assumes that it will return a 200 response
-	assert r.json()['name'] == namee
-	assert r.json()['description'] == descriptione
+	pname = "PUT examplename"
+	pdescriptione = "PUT exampledesc"
+	r = requests.put(url + '/api/v1/products/' + str(pid),
+			json = {'name': pname,
+				'description': pdescription})
+	r = requests.get(url + '/api/v1/products/' + str(pid))
+	assert r.status_code == 200
+	assert r.json()['name'] == pname
+	assert r.json()['description'] == pdescription
 
 def test_product_delete():
-	r = requests.delete(url+'/api/v1/products/'+str(idd)) # Assumses that it has a path of "/"
-	assert r.status_code == 200 # Assumes that it will return a 200 response
-	r = requests.get(url+'/api/v1/products/'+str(idd))
+	r = requests.delete(url + '/api/v1/products/'+str(pid))
+	assert r.status_code == 200
+	r = requests.get(url + '/api/v1/products/' + str(pid))
 	assert r.status_code == 404
